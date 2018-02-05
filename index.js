e94ec30d50b3d9fbc54867241d5566e999fd8d11
tonis2 <tonis.anton18@gmail.com>
2018-02-05 15:20:30 +0100

    update

--- index.js
+++ index.js
@@ -8,30 +8,38 @@ class Component extends HTMLElement {
   connectedCallback() {
     this.render();
   }
+  log() {
+    console.log('works')
+  }
 
   render() {
-    let testDom = document.createElement("div");
-    testDom.innerHTML = `<span>te</span>
-                         <span>pask</span>
-                         <span>sitt</span>`;
-
     let value = "juurvili";
+    let testDom = html`<div>
+                        <span onclick="${this.log}">te</span>
+                         <span>pask</span>
+                         <section id="test">
+                           <span>${value}</span>
+                          <div id="test2">
+                            <p>${value}</p>
+                          </div>
+                         </section>
+                       </div>`;
 
-    
-    let body = html`<div>
-                      <div>
-                        <h3>Tere</h3>
-                        <span>Pask</span>
-                      </div>
-                        <div id="test">
-                          <h3>test</h3>
-                          <span>${value}</span>
-                          ${testDom}
-                        </div>
-                      </div>
-                    `;
-
-    body.render(this);
+      console.log(testDom)
+    // let body = html`<div>
+    //                   <div>
+    //                     <h3>Tere</h3>
+    //                     <span>Pask</span>
+    //                   </div>
+    //                     <div id="test">
+    //                       <h3>test</h3>
+    //                       <span>${value}</span>
+    //                       ${testDom.dom}
+    //                     </div>
+    //                   </div>
+    //                 `;
+    //
+    //     body.render(this);
   }
 }
 
--- parser.js
+++ parser.js
@@ -1,7 +1,9 @@
+import { UUID } from "../helpers/math.js";
+
 String.prototype.html = function() {
   let parser = new DOMParser();
   let doc = parser.parseFromString(this, "text/html");
-  return doc.body.children;
+  return doc.body.children[0];
 };
 
 class HTML {
@@ -9,55 +11,33 @@ class HTML {
     this.strings = strings;
     this.values = [...values];
     this.dom = this.parseHTML();
-
     this.render = this.render.bind(this);
   }
 
   render(container) {
     let domElements = this.dom;
-    for (let element in domElements) {
-      let elementNode = domElements[element];
-      if (elementNode.nodeType == 1) {
-        container.appendChild(elementNode);
-      }
-    }
+    container.appendChild(domElements);
   }
 
   parseHTML() {
     let { values, strings } = this;
+    let valuesMap = new Map();
 
-    let domifyStrings = strings.reduce((previous, current, index) => {
+    let newStrings = strings
+      .map((string, index) => {
+        let id = UUID();
+        valuesMap.set(id, values[index]);
+        let newString =
+          index + 1 == strings.length
+            ? string
+            : `${string} <template id=${id}> </template>`;
+        return newString;
+      })
+      .reduce((prev, current) => {
+        return prev + current;
+      });
 
-      let html, element, value = values[index - (strings.length - 2)];
-      let lastChild = null;
-
-      html = typeof previous == "string" ? previous.html() : previous;
-
-
-      if (value) {
-        lastChild = html[html.length - 1];
-        if (value.nodeType !== 1) {
-          lastChild.removeChild(
-            lastChild.childNodes[lastChild.childNodes.length - 1]
-          );
-        }
-      }
-
-      if (value.nodeType == 1) {
-        element = value;
-      } else {
-        let elementType = current.replace("</", "").replace(">", "");
-        element = `<${elementType}>${value}</${elementType}>`.html();
-      }
-
-      if (lastChild) {
-        lastChild.appendChild(element.length > 0 ? element[0] : element);
-      }
-
-      return html;
-    });
-
-    return domifyStrings;
+      console.log(newStrings.html())
   }
 }
 
