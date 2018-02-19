ef8566789584c03ce38eed0eea7421908bb5e5ad
tonis2 <tonis.anton18@gmail.com>
2018-02-19 14:41:21 +0100

    shorten uuid

--- helpers/math.js
+++ helpers/math.js
@@ -2,5 +2,5 @@ export const UUID = () => {
     let S4 = () => {
        return (((1+Math.random())*0x10000)|0).toString(16).substring(1);
     };
-    return "p"+(S4()+S4()+"-"+S4()+"-"+S4()+"-"+S4()+"-"+S4()+S4()+S4());
+    return "p"+(S4()+S4()+"-"+S4());
 }
